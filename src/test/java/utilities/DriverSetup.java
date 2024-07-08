4cd438176c9d62878ef7786975f295158921d0bd
Shobuj Das <dasshobuj@gmail.com>
2024-07-08 21:48:52 +0600

    packages created

---
+++ src/test/java/utilities/DriverSetup.java
@@ -0,0 +1,11 @@
+package utilities;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class DriverSetup {
+    @BeforeSuite
+    public void openBrowser(){
+        
+    }
+}
